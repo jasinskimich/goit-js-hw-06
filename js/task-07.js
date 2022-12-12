55d50c9a67f129338d284251bfe510a218b59e96
Michał Jasiński <jasinskimichal.1996@gmail.com>
2022-12-12 20:03:42 +0100

    changes

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
-const inputSlide = document.querySelector('#font-size-control');
-const activeText = document.querySelector('#text');
+const inputSlide = document.querySelector("#font-size-control");
+const activeText = document.querySelector("#text");
+activeText.style.fontSize = "56px";
 
-inputSlide.addEventListener('input', (event) => {
-let slideAmount = event.currentTarget.value;
-activeText.style.fontSize = slideAmount + "px";
-})
+inputSlide.addEventListener("input", (event) => {
+  let slideAmount = event.currentTarget.value;
+  activeText.style.fontSize = slideAmount + "px";
+});
--- js/task-08.js
+++ js/task-08.js
@@ -5,12 +5,16 @@ loginForm.addEventListener("submit", submitForm);
 function submitForm(event) {
   event.preventDefault();
   const {
-    elements: { email, password }
-  } = event.currentTarget; 
+    elements: { email, password },
+  } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
     window.alert("Please fill in all the fields!");
+  } else {
+    const user = new Object();
+    user.email = email.value;
+    user.password = password.value;
+    console.log(user);
   }
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 }
